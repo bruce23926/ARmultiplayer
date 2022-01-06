11b475bede6e59a4d5be0a7761a5d5474cbd22a5
bruce23926 <66370857+bruce23926@users.noreply.github.com>
2022-01-07 00:49:46 +0800

    Add files via upload

---
+++ Assets/Scripts/ShootScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+    public GameObject arCamera;
+    public GameObject smoke;
+
+    public void Shoot(){
+        RaycastHit hit;
+        if(Physics.Raycast(arCamera.transform.position,arCamera.transform.forward,out hit)){
+            if (hit.transform.name == "balloon1(Clone)"|| hit.transform.name == "balloon2(Clone)"||hit.transform.name == "balloon3(Clone)"){
+                Destroy(hit.transform.gameObject);
+                Instantiate(smoke,hit.point,Quaternion.LookRotation(hit.normal));
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
